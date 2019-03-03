431d38c457d8247e265d34abe936e73e1b8303df
Kari O'Neal <triadfirewood@gmail.com>
2019-03-03 21:57:29 +0000

    Automatically backed up by Learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -1,16 +1,17 @@
 class Appointment
-  attr_accessor :patient, :doctor, :date
+  attr_accessor :patient, :doctor, :date 
   
   @@all = []
   
-  def initialize(patient, doctor, date = " - , - ")
+  def initialize(patient, doctor, date)
     @patient = patient
     @doctor = doctor
     @date = date
-    @@all << self 
-  end
+    @@all << self
+  end 
   
-  def self.all 
+  def self.all
     @@all 
   end
+  
 end
--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,31 +1,32 @@
 class Doctor
-
+  
   attr_accessor :name
   
   @@all = []
   
   def initialize(name)
-    @name = name 
+    @name = name
     @@all << self
   end
   
-  def self.all 
+  def self.all
     @@all 
-  end 
+  end
   
   def new_appointment(patient, date)
     Appointment.new(patient, self, date)
   end
   
   def appointments
-    Appointment.all.select do |appointment|
-      appointment.doctor == self
+    Appointment.all.select do |appointment| 
+    appointment.doctor == self
     end
   end
   
-  def patients 
-    appointments.map do |appointment| 
+  def patients
+    appointments.map do |appointment|
       appointment.patient
     end
   end
+  
 end
